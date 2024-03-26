0e2f393857deae7f5201bcdec7ff4c7601b6706f
Tony Lam <63186670+lamtonylam@users.noreply.github.com>
2024-03-26 21:41:18 +0200

    creating overall patches is done

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tony"
 db = SQLAlchemy(app)
+
+
 
 
 
@@ -13,10 +16,12 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-
-@app.route("/sisaankirjautunut")
+@app.route("/kirjautunut")
 def kirjautnut():
-    return render_template("kirjautunut.html")
+    query = text('SELECT nimi, hankintapaikka FROM Merkit;')
+    result = db.session.execute(query)
+    results = result.fetchall()
+    return render_template("kirjautunut.html", results=results)
 
 @app.route("/new")
 def new():
@@ -24,11 +29,26 @@ def new():
 
 @app.route("/send", methods=["POST"])
 def send():
-    nimi = request.form["nimi"]
-    tapahtuma_vai_ostettu = request.form["tapahtuma_vai_ostettu"]
+    nimi = request.form.get("nimi")
+    hankintapaikka = request.form.get("hankintapaikka")
 
-    sql = "INSERT INTO Merkit(nimi, tapahtuma_vai_ostettu) VALUES (:nimi, :tapahtuma_vai_ostettu)"
-    db.session.execute(sql, {"nimi":nimi, "tapahtuma_vai_ostettu":tapahtuma_vai_ostettu})
+    sql = text("INSERT INTO Merkit(nimi, hankintapaikka) VALUES (:nimi, :hankintapaikka)")  
+    db.session.execute(sql, {"nimi": nimi, "hankintapaikka": hankintapaikka})
     db.session.commit()
 
     return redirect("/sisaankirjautunut")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Merkit (
+    id SERIAL PRIMARY KEY, 
+    nimi TEXT, 
+    hankintapaikka TEXT
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE usersTOmerkit (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    merkki_id INTEGER REFERENCES Merkit(id),  
+);
