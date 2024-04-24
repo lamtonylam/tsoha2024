1364927a8793e98d5f5f199fc41230442f403512
Tony Lam <tony.lam@iki.fi>
2024-04-24 11:41:29 +0300

    Feature: add 404 not found page

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ It also sets up the secret key for the application from an environment variable.
 
 from os import getenv
 from flask import Flask
+from flask import send_file
+import requests
+from io import BytesIO
 
 
 app = Flask(__name__)
@@ -12,3 +15,8 @@ app.secret_key = getenv("SECRET_KEY")
 
 
 import routes
+
+@app.errorhandler(404)
+def page_not_found(e):
+    response = requests.get('https://http.cat/images/404.jpg')
+    return send_file(BytesIO(response.content), mimetype='image/jpeg')
